0a9c052e915e7490490f500773d91b1b55fe68b0
VIET TRI LE <leviettri88@gmail.com>
2022-11-25 22:14:01 -0600

    Comment version3(tested Swagger)

---
+++ comment-service/src/main/java/com/edu/miu/config/rabbitmq/RabbitMQTopicConfig.java
@@ -0,0 +1,26 @@
+package com.edu.miu.config.rabbitmq;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQTopicConfig {
+
+    @Bean
+    TopicExchange topicExchange() {
+        return new TopicExchange("topic-exchange");
+    }
+
+    @Bean
+    Binding commentMovieQueueBinding(Queue commentMovieQueue, TopicExchange topicExchange) {
+        return BindingBuilder
+                .bind(commentMovieQueue)
+                .to(topicExchange)
+                .with("comment-movie-queue");
+    }
+
+}
---
+++ comment-service/src/main/java/com/edu/miu/repo/CommentRepo.java
@@ -0,0 +1,29 @@
+package com.edu.miu.repo;
+
+import com.edu.miu.entity.Comment;
+import com.edu.miu.enums.MediaType;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepo extends CrudRepository<Comment,Integer> {
+    List <Comment> findAllByUserId(int id);
+    Comment findByUserId(int id);
+    List<Comment> findAllByMediaId(int id);
+    List<Comment> findAllByUserIdAndMediaId(int usersId,int mediaId);
+    void deleteByUserId(int id);
+
+
+    @Modifying
+    @Query("DELETE FROM Comment c WHERE c.mediaId=:mediaId AND c.mediaType=:mediaType")
+    void deleteAllByMediaIdAndMediaType(@Param("mediaId") int mediaId,MediaType mediaType);
+
+    @Modifying
+    @Query("DELETE FROM Comment c WHERE c.userId=:userId")
+    void deleteAllByUserId (int userId);
+}
